8193c4d9af946c3aea346a6eb3555b33b1d55463
Anastasia Kalashnikova <nastena03092004@gmail.com>
2025-05-15 02:22:37 +0300

    Добавление ограничения выбора цвета

--- kittygram/cats/models.py
+++ kittygram/cats/models.py
@@ -1,10 +1,19 @@
 from django.db import models # type: ignore
 
+CHOICES = (
+        ('Gray', 'Серый'),
+        ('Black', 'Чёрный'),
+        ('White', 'Белый'),
+        ('Ginger', 'Рыжий'),
+        ('Mixed', 'Смешанный'),
+    )
+
 class Achievement(models.Model):
     name = models.CharField(max_length=64)
 
     def __str__(self):
         return self.name
+
 
 class Owner(models.Model):
     first_name = models.CharField(max_length=128)
@@ -16,16 +25,17 @@ class Owner(models.Model):
 
 class Cat(models.Model):
     name = models.CharField(max_length=16)
-    color = models.CharField(max_length=16)
+    color = models.CharField(max_length=16, choices=CHOICES)
     birth_year = models.IntegerField()
     owner = models.ForeignKey(
-        Owner, related_name='cats', on_delete=models.CASCADE)
-    achievements = models.ManyToManyField(
-        Achievement, through='AchievementCat')
+        Owner, related_name='cats', on_delete=models.CASCADE, blank=True, 
+        null=True)
+    achievements = models.ManyToManyField(Achievement, through='AchievementCat')
 
     def __str__(self):
         return self.name 
     
+
 class AchievementCat(models.Model):
     achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
     cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
--- kittygram/cats/serializers.py
+++ kittygram/cats/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers # type: ignore
 import datetime as dt
 import webcolors # type: ignore
 
-from .models import Cat, Owner, Achievement, AchievementCat
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
 
 class Hex2NameColor(serializers.Field):
     def to_representation(self, value):
@@ -15,15 +15,17 @@ class Hex2NameColor(serializers.Field):
         return data
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
     age = serializers.SerializerMethodField()
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
