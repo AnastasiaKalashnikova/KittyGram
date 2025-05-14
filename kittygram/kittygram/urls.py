83cdceb70c2e33a4edebf48f6e5ee93cbb8f0dcf
Anastasia Kalashnikova <nastena03092004@gmail.com>
2025-05-15 00:03:52 +0300

    Viewset

--- kittygram/kittygram/urls.py
+++ kittygram/kittygram/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+# urls.py
+from rest_framework.routers import SimpleRouter
 
-from cats.views import CatList, CatDetail
+from django.urls import include, path
+
+from cats.views import CatViewSet
+
+router = SimpleRouter()
+router.register('cats', CatViewSet)
+
 
 urlpatterns = [
-    path('cats/', CatList.as_view()),
-    path('cats/<int:pk>/', CatDetail.as_view()),
-] 
-
-
+    path('', include(router.urls)),
+]
